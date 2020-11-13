b460dcca21b941257c0ec361e93410ea9eab88d3
yura159 <47238316+yura159@users.noreply.github.com>
2020-11-14 00:55:06 +0500

    Обработка заполнения графа

--- ChatBot/ChatBot/Graff.cs
+++ ChatBot/ChatBot/Graff.cs
@@ -25,36 +25,71 @@ namespace ChatBot
 
         private List<Element> n(Dictionary<string, List<string>> info)
         {
-            //var res = new List<Element>();
-            //var one = info
-            //    .Where(x => SelectOne(info, x))
-            //    .ToDictionary(x => x.Key, x => x.Value);
-            //var other = info
-            //    .Where(x => !SelectOne(info, x));
-            //if (other == null)
-            //{
-            //    foreach (var e in one)
-            //    {
-            //        res.Add(new End(e.Value.FirstOrDefault()));
-            //    }
-            //    return res;
-            //}
-            //other = other.ToDictionary(x => x.Key, x => x.Value);
-            //foreach (var e in one)
-            //{
-            //    //foreach (var v in e.Value)
-            //    //{
-            //        res.Add(new Medium(n(other.ToDictionary(x => x.Key, x => x.Value)), e.Key));
-            //    //}
-            //}
-            //return res;
-        } 
+            var result = new List<Element>();
+            var node = info
+                .Where(x => SelectOne(info, x.Key))
+                .ToDictionary(x => x.Key, x => x.Value);
 
-        private static bool SelectOne(Dictionary<string, List<string>> info, KeyValuePair<string, List<string>> x)
+            var endNode = CreateEndNode(info);
+
+            return result;
+        }
+
+        private List<Element> CreateEndNode(Dictionary<string, List<string>> info)
+        {
+            var keys = info.Keys.ToList();
+            var endNode = new List<Element>();
+            GetNode(keys, info, endNode);
+            return endNode;
+        }
+
+        private void GetNode(List<string> keys, Dictionary<string, List<string>> info, List<Element> endNode)
+        {
+            foreach (var key in keys)
+            {
+                foreach(var value in info.Values)
+                {
+                    if (value.Contains(key))
+                        GetNode(value, info, endNode);
+                    else if (!SelectOne(info, key))
+                        endNode.Add(new End(key));
+                }
+            }
+        }
+
+        /*
+        private List<Element> n(Dictionary<string, List<string>> info)
+        {
+            var res = new List<Element>();
+            var one = info
+                .Where(x => SelectOne(info, x.Key))
+                .ToDictionary(x => x.Key, x => x.Value);
+            var other = info
+                .Where(x => !SelectOne(info, x.Key));
+            if (other is null)
+            {
+                foreach (var e in one)
+                {
+                    res.Add(new End(e.Value.FirstOrDefault()));
+                }
+                return res;
+            }
+            other = other.ToDictionary(x => x.Key, x => x.Value);
+            foreach (var e in one)
+            {
+                //foreach (var v in e.Value)
+                //{
+                res.Add(new Medium(n(other.ToDictionary(x => x.Key, x => x.Value)), e.Key));
+                //}
+            }
+            return res;
+        } 
+        */
+        private static bool SelectOne(Dictionary<string, List<string>> info, string x)
         {
             foreach (var Ls in info.Values)
             {
-                if (Ls.Contains(x.Key))
+                if (Ls.Contains(x))
                     return false;
             }
             return true;
