9ab0c9e0378e5d3b36bbccd6f3402c28a124d3e7
mishafominih <mishagjxn@mail.ru>
2020-11-14 05:25:53 +0500

    ...

--- ChatBot/ChatBot/NeuralNetwork.cs
+++ ChatBot/ChatBot/NeuralNetwork.cs
@@ -4,16 +4,46 @@ using System.Xml;
 using System.IO;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChatBot
 {
     class NeuralNetwork
     {
-		Neuron[] start;
-		List<Neuron> hideLayer
+		Neuron[] startLayer;
+		List<Neuron[]> hideLayer;
+		Neuron[] endLayer;
 		public NeuralNetwork(int StartSize, int countHide, int[] arrCountHide, int EndSize)
 		{
+			startLayer = new Neuron[StartSize];
+			for(int i = 0; i < countHide; i++)
+            {
+				hideLayer.Add(new Neuron[arrCountHide[i]]);
+            }
+			endLayer = new Neuron[EndSize];
+			Initialise();
+		}
 
+		private void Initialise()
+        {
+			for(int i = 0; i < startLayer.Length; i++)
+            {
+				startLayer[i] = new Neuron(0);
+			}
+			for(int j = 0; j < hideLayer.Count; j++)
+            {
+				for (int i = 0; i < hideLayer[j].Length; i++)
+				{
+					if(i == 0)
+						hideLayer[j][i] = new Neuron(startLayer.Length);
+					else
+						hideLayer[j][i] = new Neuron(hideLayer[j].Length);
+				}
+			}
+			for (int i = 0; i < endLayer.Length; i++)
+			{
+				endLayer[i] = new Neuron(hideLayer.Last().Length);
+			}
 		}
 	}
 }
--- ChatBot/ChatBot/Neuron.cs
+++ ChatBot/ChatBot/Neuron.cs
@@ -9,61 +9,17 @@ namespace ChatBot
 {
     public class Neuron
     {
-
-		/**
-		 * Конструктор нейрона, создает веси и устанавливает случайные значения
-		 */
-		public Neuron()
+		static Random rand = new Random();
+		double[] weight;
+		public Neuron(int count)
 		{
-
+			weight = new double[count];
+			for(int i = 0; i < count; i++)
+            {
+				weight[i] = rand.Next();
+            }
 		}
 
-		/**
-		 * ответы нейронов, жесткая пороговая
-		 * @param input - входной вектор
-		 * @return ответ 0 или 1
-		 */
-		public double transferHard(double[,] input)
-		{
 
-		}
-
-		/**
-		 * ответы нейронов с вероятностями
-		 * @param input - входной вектор
-		 * @return n вероятность
-		 */
-		public double transfer(double[,] input)
-		{
-
-		}
-
-		/**
-		 * устанавливает начальные произвольные значения весам 
-		 */
-		void randomizeWeights()
-		{
-
-		}
-
-		/**
-		 * изменяет веса нейронов
-		 * @param input - входной вектор
-		 * @param d - разница между выходом нейрона и нужным выходом
-		 */
-		public void changeWeights(double[,] input, double d)
-		{
-
-		}
-
-		public void prepareForSerialization()
-		{
-
-		}
-
-		public void onDeserialize()
-		{
-
-		}
 	}
 }
