b3f695b37a197c9a64703bd0f0b00459b707528f
yura159 <47238316+yura159@users.noreply.github.com>
2020-11-14 19:23:10 +0500

    добавление листа значений

--- ChatBot/ChatBot/ProcessingString.cs
+++ ChatBot/ChatBot/ProcessingString.cs
@@ -12,15 +12,40 @@ namespace ChatBot
         {
             var result = new List<string>();
             var word = new StringBuilder();
-            foreach(var ch in line.ToLower())
+            foreach (var ch in line.ToLower())
             {
                 if (Char.IsLetter(ch))
                     word.Append(ch);
-                else if(word.Length != 0)
+                else if (word.Length != 0)
                 {
                     result.Add(word.ToString());
                     word.Clear();
                 }
+            }
+            return result;
+        }
+
+        public static List<double> StringEquals(List<string> current, List<string> val2)
+        {
+
+            var result = new List<double>();
+            foreach(var e1 in current)
+            {
+                Tuple<string, string> tuple;
+                var max = 0.0;
+                var endWord = "";
+                foreach(var e2 in val2)
+                {
+                    var x = WorkServer.GetData(e1, e2);
+                    if (x > max)
+                    {
+                        max = x;
+                        tuple = new Tuple<string, string>(e1, e2);
+                        endWord = e2;
+                    }
+                }
+                val2.Remove(endWord);
+                result.Add(max);
             }
             return result;
         }
