78c94864d624c386a96ee34113cfcad1cce2b879
yura159 <47238316+yura159@users.noreply.github.com>
2020-11-14 16:44:04 +0500

    Работа с сервером

---
+++ ChatBot/ChatBot/WorkServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    public static class WorkServer
+    {
+        public static double GetData(string current, string comparison)
+        {
+            return 0;
+        }
+    }
+}
